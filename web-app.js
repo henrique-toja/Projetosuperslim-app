b9003b8701d9f080ff277fa98a82d7d79d50c000
Henrique Toja <168424648+henrique-toja@users.noreply.github.com>
2024-11-24 08:24:43 -0300

    Atualizar o web-app.js

--- web-app.js
+++ web-app.js
@@ -5,6 +5,17 @@ let deferredPrompt;
 // Evento beforeinstallprompt para detectar a possibilidade de instalação do PWA
 window.addEventListener('beforeinstallprompt', (event) => {
 
+// Exibe o botão de instalação
+  const installButton = document.getElementById('install');
+  if (installButton) {
+    installButton.style.display = 'block'; // Garantir que o botão seja visível
+  }
+
+// Adiciona o comportamento de clique ao botão de instalação
+  installButton.addEventListener('click', () => {
+    // Mostra o prompt de instalação quando o botão é clicado
+    installPromptEvent.prompt();
+
 // Aguarda a resposta do usuário (aceitar ou recusar a instalação)
       installPromptEvent.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
@@ -13,15 +24,10 @@ window.addEventListener('beforeinstallprompt', (event) => {
           console.log('Usuário recusou a instalação.');
         }
 
-// Bloquear o "pull-to-refresh" apenas no PWA
-document.addEventListener('touchmove', function(event) {
-  const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
-
-  // Impedir o pull-to-refresh quando estiver no topo da página, mas apenas no PWA
-  if (isStandalone && window.scrollY === 0 && event.touches[0].clientY > 0) {
-    event.preventDefault(); // Bloqueia o refresh
-  }
-}, { passive: false });
+// O botão continua visível mesmo após a resposta
+    });
+  });
+});
 
 // Registrar o Service Worker
 if ('serviceWorker' in navigator) {
@@ -34,16 +40,15 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-  // Salva o evento para ser acionado manualmente mais tarde
+// Salva o evento para ser acionado manualmente mais tarde
   installPromptEvent = event;
 
-  // Exibe o botão de instalação
-  const installButton = document.getElementById('install');
-  if (installButton) {
-    installButton.style.display = 'block'; // Garantir que o botão seja visível
-  }
+// Bloquear o "pull-to-refresh" apenas no PWA
+document.addEventListener('touchmove', function(event) {
+  const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
 
-  // Adiciona o comportamento de clique ao botão de instalação
-  installButton.addEventListener('click', () => {
-    // Mostra o prompt de instalação quando o botão é clicado
-    installPromptEvent.prompt();
+// Impedir o pull-to-refresh quando estiver no topo da página, mas apenas no PWA
+  if (isStandalone && window.scrollY === 0 && event.touches[0].clientY > 0) {
+    event.preventDefault(); // Bloqueia o refresh
+  }
+}, { passive: false });
